41259afa396e5bfa08f676293ddcbbb35b2f75d7
LAPTOP-BP7K41TK\82103 <rnjstndus49@gmail.com>
2023-03-09 22:59:18 +0900

    add column, import.sql, add thymeleaf

--- src/main/java/flow/fileextension/entity/FileExtension.java
+++ src/main/java/flow/fileextension/entity/FileExtension.java
@@ -11,8 +11,10 @@ public class FileExtension extends BaseEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long fileExtensionSeq;
 
+    private String fileExtensionName;
+
     @Enumerated(EnumType.STRING)
-    private FileExtensionType fileExtensionName;
+    private FileExtensionType fileExtensionType;
 
     @Enumerated(EnumType.STRING)
     private FileExtensionVisibility fileExtensionVisibility;
