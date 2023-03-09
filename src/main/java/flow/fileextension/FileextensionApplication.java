9d6b9f2a6bdc573bbf5cd5a9b46b980b3490c5d4
LAPTOP-BP7K41TK\82103 <rnjstndus49@gmail.com>
2023-03-09 10:46:15 +0900

    Initial commit

---
+++ src/main/java/flow/fileextension/FileextensionApplication.java
@@ -0,0 +1,13 @@
+package flow.fileextension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileextensionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileextensionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flow/fileextension/FileextensionApplicationTests.java
@@ -0,0 +1,13 @@
+package flow.fileextension;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileextensionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
