51e0383b63ed115cc6d9ab2ada6732e9d73d3e66
LAPTOP-BP7K41TK\82103 <rnjstndus49@gmail.com>
2023-03-12 03:41:32 +0900

    war, version

--- src/main/java/flow/fileextension/FileextensionApplication.java
+++ src/main/java/flow/fileextension/FileextensionApplication.java
@@ -2,14 +2,20 @@ package flow.fileextension;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class FileextensionApplication {
+public class FileextensionApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FileextensionApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
+	}
 }
